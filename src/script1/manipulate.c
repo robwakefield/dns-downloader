82075dc4d434aa552e0188b8ed92881140ff0541
rob <rgw20@ic.ac.uk>
2023-09-23 18:17:07 +0100

    Get args in manipulate

--- src/script1/manipulate.c
+++ src/script1/manipulate.c
@@ -2,13 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
+int main(int argc, char *argv[]) {
 
-int main(void)
-{
-  /* 
-    Read in filenames and urls from urls.txt
-  */
+    if (argc != 2) {
+      printf("Incorrect number of arguments passed: %d\n", argc);
+      return 1;
+    }
 
-  
-  return 0;
+    char *fname = argv[1];
+    printf("Manipulating %s\n", fname);
+
+    // Open file and do manipulations
+
+    return 0;
 }
