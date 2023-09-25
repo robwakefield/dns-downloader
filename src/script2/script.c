afe169703c24dc374bae5b742a86aff42600f501
rob <rgw20@ic.ac.uk>
2023-09-25 21:06:35 +0100

    Copy manipulated files into /etc/bind/master

--- src/script2/script.c
+++ src/script2/script.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <dirent.h>
+#include <limits.h>
+#include <sys/stat.h>
 
 #define MAX_URLS 100
 #define MAX_LINE_LENGTH 512
@@ -9,6 +13,44 @@
 #define MAX_URL_LENGTH 256
 
 #define COMMENT_CHAR '#'
+
+#define BUFFER_SIZE 1024
+
+void copyFile(const char *srcPath, const char *destPath) {
+    int srcFile, destFile;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytesRead;
+
+    srcFile = open(srcPath, O_RDONLY);
+    if (srcFile == -1) {
+        perror("Failed to open source file");
+        return;
+    }
+
+    destFile = open(destPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (destFile == -1) {
+        perror("Failed to open destination file");
+        close(srcFile);
+        return;
+    }
+
+    while ((bytesRead = read(srcFile, buffer, BUFFER_SIZE)) > 0) {
+        if (write(destFile, buffer, bytesRead) != bytesRead) {
+            perror("Write error");
+            break;
+        }
+    }
+
+    close(srcFile);
+    close(destFile);
+
+    if (bytesRead == -1) {
+        perror("Read error");
+        return;
+    }
+
+    printf("Copied: %s -> %s\n", srcPath, destPath);
+}
 
 int main(void)
 {
@@ -129,6 +171,38 @@ int main(void)
 
   // Close the directory
   closedir(dir);
+
+  /*
+    Move files from ./downloads to user defined directory
+  */
   
+  const char *srcDir = "./downloads";
+  const char *destDir = "/etc/bind/master";
+
+  // Open the source directory
+  dir = opendir(srcDir);
+  if (dir == NULL) {
+      perror("Failed to open source directory");
+      return 1;
+  }
+
+  // Create the destination directory if it doesn't exist
+  mkdir(destDir, 0755);
+
+  // Iterate over files in the source directory
+  while ((entry = readdir(dir)) != NULL) {
+      if (entry->d_type == DT_REG) { // Regular file
+          char srcPath[PATH_MAX];
+          char destPath[PATH_MAX];
+
+          snprintf(srcPath, sizeof(srcPath), "%s/%s", srcDir, entry->d_name);
+          snprintf(destPath, sizeof(destPath), "%s/%s", destDir, entry->d_name);
+
+          copyFile(srcPath, destPath);
+      }
+  }
+
+  closedir(dir);
+
   return 0;
 }
