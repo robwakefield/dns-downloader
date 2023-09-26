b6ba401fbf74d1e6921f3a3d22320b23418ad8f0
rob <rgw20@ic.ac.uk>
2023-09-26 15:07:01 +0100

    Extract logging to separate file

--- src/script1/manipulate.c
+++ src/script1/manipulate.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <time.h>
+#include "lg.c"
+
 
 #define MAX_URLS 100
 #define MAX_LINE_LENGTH 512
@@ -11,42 +11,6 @@
 
 #define COMMENT_CHAR '#'
 
-void lg(const char *format, ...) {
-    // Get the current time and date
-    time_t rawtime;
-    struct tm *timeinfo;
-    char timeStr[32]; // Adjust the size based on your formatting needs
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    strftime(timeStr, sizeof(timeStr), "[%Y-%m-%d %H:%M:%S] ", timeinfo);
-
-    // Print to the terminal
-    printf("%s", timeStr);
-
-    // Print to the log file
-    FILE *logfile = fopen("./log.txt", "a");
-    if (logfile == NULL) {
-        perror("Failed to open log file");
-        return;
-    }
-    fprintf(logfile, "%s", timeStr);
-
-    // Print the formatted message to both the terminal and the log file
-    va_list args;
-    va_start(args, format);
-    vfprintf(stdout, format, args); // Print to terminal
-    vfprintf(logfile, format, args); // Print to log file
-    va_end(args);
-
-    // Append a newline character to the log file
-    fprintf(logfile, "\n");
-    // Append a newline character to stdout
-    fprintf(stdout, "\n");
-
-    // Close the log file
-    fclose(logfile);
-}
 
 void modifyDNSRecord(char *input, int newValue) {
     // Tokenize string on ' 's
@@ -106,7 +70,7 @@ int main(int argc, char *argv[]) {
 
     char *fname = argv[1];
     int increment = 0;
-    
+
     /* 
       Check in urls.txt for increment value
     */
--- src/script1/script.c
+++ src/script1/script.c
@@ -6,8 +6,7 @@
 #include <dirent.h>
 #include <limits.h>
 #include <sys/stat.h>
-#include <stdarg.h>
-#include <time.h>
+#include "lg.c"
 
 #define MAX_URLS 100
 #define MAX_LINE_LENGTH 512
@@ -17,44 +16,6 @@
 #define COMMENT_CHAR '#'
 
 #define BUFFER_SIZE 1024
-
-// TODO: update log style to SUCCESS/FAIL/ERROR/INFO
-void lg(const char *format, ...) {
-    // Get the current time and date
-    time_t rawtime;
-    struct tm *timeinfo;
-    char timeStr[32]; // Adjust the size based on your formatting needs
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    strftime(timeStr, sizeof(timeStr), "[%Y-%m-%d %H:%M:%S] ", timeinfo);
-
-    // Print to the terminal
-    printf("%s", timeStr);
-
-    // Print to the log file
-    FILE *logfile = fopen("./log.txt", "a");
-    if (logfile == NULL) {
-        perror("Failed to open log file");
-        return;
-    }
-    fprintf(logfile, "%s", timeStr);
-
-    // Print the formatted message to both the terminal and the log file
-    va_list args;
-    va_start(args, format);
-    vfprintf(stdout, format, args); // Print to terminal
-    vfprintf(logfile, format, args); // Print to log file
-    va_end(args);
-
-    // Append a newline character to the log file
-    fprintf(logfile, "\n");
-    // Append a newline character to stdout
-    fprintf(stdout, "\n");
-
-    // Close the log file
-    fclose(logfile);
-}
 
 void copyFile(const char *srcPath, const char *destPath) {
     int srcFile, destFile;
