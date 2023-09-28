a7f67cfb5f84793388cb9694644a0846ff424e5a
rob <rgw20@ic.ac.uk>
2023-09-28 13:42:37 +0100

    Incorporate manipulate function into script2

--- src/script2/script2.c
+++ src/script2/script2.c
@@ -7,7 +7,6 @@
 #include <limits.h>
 #include <sys/stat.h>
 #include "lg.c"
-#include "manipulate2.c"
 
 #define DEST_DIR "/etc/bind/master"
 
@@ -19,6 +18,126 @@
 #define COMMENT_CHAR '#'
 
 #define BUFFER_SIZE 1024
+
+int manipulate(char *fname) {
+
+    int increment = 0;
+
+    /* 
+      Check in urls.txt for increment value
+    */
+
+    FILE *file;
+    char line[MAX_URL_LENGTH];
+
+    file = fopen("urls.txt", "r");
+
+    if (file == NULL) {
+      lg("Unable to open urls.txt");
+      perror("Unable to open urls.txt");
+      return 1;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if (line[0] != COMMENT_CHAR) {
+          char dir_name[MAX_LINE_LENGTH];
+          strcpy(dir_name, "./downloads/");
+          char name[MAX_FNAME_LENGTH]; // Store the filename
+          char url[MAX_URL_LENGTH]; // Store the URL
+          int inc;
+
+          // Use sscanf to split strings and get increment (if present)
+          if (sscanf(line, "%s %s %d", name, url, &inc) == 3) {
+            strcat(dir_name, name);
+            if (!strcmp(fname, dir_name)) {
+              increment = inc;
+              break;
+            }
+          } else if (sscanf(line, "%s %s", name, url) == 2) {
+            strcat(dir_name, name);
+            if (!strcmp(fname, dir_name)) {
+              increment = 0;
+              break;
+            }
+          } else {
+            continue;
+          }
+        }
+    }
+    
+    // Close the file
+    fclose(file);
+
+    /* 
+      Open file and do manipulations
+    */
+
+    // Open the original file for reading
+    FILE *originalFile = fopen(fname, "r");
+    if (originalFile == NULL) {
+        lg("Error opening the original file");
+        perror("Error opening the original file");
+        return 1;
+    }
+
+    // Open a temporary file for writing
+    FILE *tempFile = fopen("downloads/temp.txt", "w");
+    if (tempFile == NULL) {
+        lg("Error creating the temporary file");
+        perror("Error creating the temporary file");
+        fclose(originalFile);
+        return 1;
+    }
+
+    // Line number to be deleted
+    int currentLine = 1;
+
+
+    char buffer[1024]; // Adjust buffer size as needed
+
+    // Add header information
+    strcpy(buffer, "$TTL 300\n@ SOA localhost. Securedomains.rpz. ");
+    char inc_char[20]; // Assuming a maximum of 20 characters for the string
+    // Using sprintf to convert int to string
+    sprintf(inc_char, "%d", increment);
+    strcat(buffer, inc_char);
+    strcat(buffer, " 3600 3600 259200 300\n  NS  localhost.\n");
+
+    fputs(buffer, tempFile);
+
+    // Read and copy lines from the original file to the temporary file
+    while (fgets(buffer, sizeof(buffer), originalFile) != NULL) {
+        
+        // Delete all lines that start with #
+        if (buffer[0] != '#' && buffer[0] != '\n') {
+          // Add " CNAME ." to end of line
+          buffer[strcspn(buffer, "\n")] = 0; // Remove newline
+          strcat(buffer, " CNAME .\n");
+          fputs(buffer, tempFile);
+        }
+        currentLine++;
+    }
+
+    // Close both files
+    fclose(originalFile);
+    fclose(tempFile);
+
+    // Delete the original file
+    if (remove(fname) != 0) {
+        lg("Error deleting the original file");
+        perror("Error deleting the original file");
+        return 1;
+    }
+
+    // Rename the temporary file to the original file
+    if (rename("downloads/temp.txt", fname) != 0) {
+        lg("Error renaming the temporary file");
+        perror("Error renaming the temporary file");
+        return 1;
+    }
+
+    return 0;
+}
 
 void copyFile(const char *srcPath, const char *destPath) {
     int srcFile, destFile;
